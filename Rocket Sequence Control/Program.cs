6e5cc807fa26c920e10783721554e77e2bdf7afd
Cameron St J <cameron@stjohn.id.au>
2019-05-20 00:05:35 +1000

    Remove sequence shooter comments.

--- Rocket Sequence Control/Program.cs
+++ Rocket Sequence Control/Program.cs
@@ -30,16 +30,6 @@ namespace IngameScript
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set RuntimeInfo.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
             Runtime.UpdateFrequency = UpdateFrequency.Update1;
             rocketList = GetRockets();
             CheckShootFrequency();
@@ -59,12 +49,6 @@ namespace IngameScript
 
         public void Save()
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
         }
 
         public void Main(string argument, UpdateType updateSource)
