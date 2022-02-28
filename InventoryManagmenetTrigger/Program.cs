d39ee8c7339853ec83304adb0b9cd7bc90bbba4b
Cameron <cameron@stjohn.id.au>
2022-02-28 17:58:29 +1000

    Inv Manager  - Lots of CHanges

--- InventoryManagmenetTrigger/Program.cs
+++ InventoryManagmenetTrigger/Program.cs
@@ -23,61 +23,71 @@ namespace IngameScript
 
         List<IMyCargoContainer> CargoContainers;
         bool wasFullFlag;
-        IMyBlockGroup triggerBlocks;
+        List<IMyFunctionalBlock> triggerBlocks;
+        string groupName = null;
 
-        IMyTextPanel outputPanel;
+        IMyTextPanel outputPanel = null;
 
         bool curState;
 
         public Program()
         {
-            if (Me.CustomData == null)
+            groupName = Me.CustomData;
+            if (Me.CustomData == null || Me.CustomData == "")
             {
                 Echo("Put the Search name in custom data of this block.  Script will search for the value in the custom data and that value with trigger.  eg. [full] and [full trigger] and lcd trigger [full]");
                 return;
             }
-
-
+            
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
-
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(groupName);
             CargoContainers = new List<IMyCargoContainer>();
+            triggerBlocks = new List<IMyFunctionalBlock>();
             List<IMyCargoContainer> invBlocks = new List<IMyCargoContainer>();
             //GridTerminalSystem.GetBlocks(invBlocks);
             List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
-
+            
+            
             GridTerminalSystem.GetBlocks(blocks);
 
             IMyCargoContainer box;
             IMyTextPanel outPanel;
+            IMyFunctionalBlock funcBlock;
             for (int i = 0; i < blocks.Count; i++)
             {
                 box = blocks[i] as IMyCargoContainer;
-                if (box != null)
+                if (box != null && box.CustomName.ToLower().Contains($"[{groupName.ToLower()}]"))
                 {
-                    if (box.CustomName.ToLower().Contains($"[{Me.CustomData.ToLower()}]")) CargoContainers.Add(box);
+                    sb.AppendLine("Found Box " + box.CustomName);
+                    CargoContainers.Add(box);
                 }
+
                 outPanel = blocks[i] as IMyTextPanel;
-                if (outPanel != null && outPanel.CustomName.ToLower() == $"lcd trigger [{Me.CustomData.ToLower()}]")
+                if (outPanel != null && outPanel.CustomName.ToLower() == $"lcd trigger [{groupName.ToLower()}]")
                 {
                     outputPanel = outPanel;
                     outputPanel.ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
                     outputPanel.FontSize = 1.5f;
+                    sb.AppendLine("Found LCD " + outPanel.CustomName);
+                }
+                funcBlock = blocks[i] as IMyFunctionalBlock;
+                if (funcBlock != null && funcBlock.CustomName.ToLower().Contains($"[{groupName.ToLower()} trigger]"))
+                {
+                    sb.AppendLine("Found Triggered Block " + funcBlock.CustomName);
+                    triggerBlocks.Add(funcBlock);
                 }
             }
-
-            //[full trig]
-            List<IMyBlockGroup> blockGroups = new List<IMyBlockGroup>();
-            GridTerminalSystem.GetBlockGroups(blockGroups);
-            for (int i = 0; i < blockGroups.Count; i++)
-            {
-                if (blockGroups[i].Name.ToLower().Contains($"[{Me.CustomData.ToLower()} trigger]")) triggerBlocks = blockGroups[i];
-            }
-
+            
+            sb.AppendLine("Box Count " + CargoContainers.Count);
+            sb.AppendLine("Triggered Blocks Count " + triggerBlocks.Count);
+            Echo(sb.ToString());
             wasFullFlag = false;
             curState = false;
 
             //Me.GetSurface(0).ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
             //Me.GetSurface(0).Alignment = VRage.Game.GUI.TextPanel.TextAlignment.LEFT;
+            
         }
 
         public void Save()
@@ -88,6 +98,7 @@ namespace IngameScript
         public void Main(string argument, UpdateType updateSource)
         {
             StringBuilder scriptstatus = new StringBuilder();
+            scriptstatus.AppendLine(groupName);
             MyFixedPoint totalVolume = 0;
             MyFixedPoint usedVolume = 0;
             IMyInventory thisInv;
@@ -101,6 +112,8 @@ namespace IngameScript
                     totalVolume += thisInv.MaxVolume;
                 }
             }
+
+            
             var fullPercent = Math.Round(((double)usedVolume / (double)totalVolume), 4);
             scriptstatus.AppendLine("Percentage: " + fullPercent * 100);
             if (fullPercent > .95)
@@ -122,27 +135,35 @@ namespace IngameScript
                 wasFullFlag = false;
             }
 
-            if (curState) scriptstatus.AppendLine(triggerBlocks.Name + ": Enabled");
-            else scriptstatus.AppendLine(triggerBlocks.Name + ": Disabled");
-            if (wasFullFlag) scriptstatus.AppendLine("Waiting To Empty");
-            else scriptstatus.AppendLine("Filling");
-
+            if (triggerBlocks.Count == 0)
+            {
+                scriptstatus.AppendLine($"Add [{groupName} trigger] to a block to have it triggered");
+            }
+            else
+            {
+                if (curState) scriptstatus.AppendLine(groupName + ": Enabled");
+                else scriptstatus.AppendLine(groupName + ": Disabled");
+            }
+            //if (wasFullFlag) scriptstatus.AppendLine("Waiting To Empty");
+            //else scriptstatus.AppendLine("Filling");
+            
             if (outputPanel == null) Echo(scriptstatus.ToString());
             else outputPanel.WriteText(scriptstatus.ToString());
         }
-
+        
         public void SetGroup(bool state)
         {
-            curState = state;
-            IMyFunctionalBlock func;
-            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
-            triggerBlocks.GetBlocks(blocks);
-            foreach (var b in blocks)
-            {
-                func = b as IMyFunctionalBlock;
-                if (func != null)
+            if (triggerBlocks.Count  > 0)
+            { 
+                curState = state;
+                IMyFunctionalBlock func;
+                foreach (var b in triggerBlocks)
                 {
-                    func.Enabled = state;
+                    func = b as IMyFunctionalBlock;
+                    if (func != null)
+                    {
+                        func.Enabled = state;
+                    }
                 }
             }
         }
